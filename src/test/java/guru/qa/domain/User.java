5237c16e0ee1c47395fcc682c5ec75d9473ff4d6
QAmilord <i@qamilord.ru>
2022-04-25 00:46:01 +0300

    lesson complete

---
+++ src/test/java/guru/qa/domain/User.java
@@ -0,0 +1,14 @@
+package guru.qa.domain;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class User {
+
+    public String name;
+    public String surname;
+    @SerializedName("favorite_music")
+    public List<String> favoriteMusic;
+    public Address address;
+}
